2dc734ccd1f3056724e7a9542cb340b86e1745d9
Rafael Lodi <rafaelcardeallodi@hotmail.com>
2022-07-09 01:40:26 -0300

    Finalização da Home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -29,6 +29,36 @@ export const FormContainer = styled.div`
   flex-wrap: wrap;
 `
 
+const BaseInput = styled.input`
+  height: 2.5rem;
+  padding: 0 0.5rem;
+
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid ${({ theme }) => theme['gray-500']};
+
+  font-weight: bold;
+  font-size: 1.125rem;
+  color: ${({ theme }) => theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${({ theme }) => theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme['gray-500']};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+
 export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
   font-size: 10rem;
@@ -53,4 +83,33 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  padding: 1rem;
+
+  border: 0;
+  border-radius: 8px;
+  background: ${({ theme }) => theme['green-500']};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  color: ${({ theme }) => theme['gray-100']};
+  font-weight: bold;
+
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:not(:disabled):hover {
+    background: ${({ theme }) => theme['green-700']};
+  }
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
 `
